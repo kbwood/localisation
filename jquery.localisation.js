676ec1cc3d58c3522ff90771fcb5dba1cade1dd4
Keith Wood <kbwood@iinet.net.au>
2012-05-05 22:10:31 +1000

    Release 1.0.2

--- jquery.localisation.js
+++ jquery.localisation.js
@@ -1,5 +1,5 @@
-/* http://home.iprimus.com.au/kbwood/jquery/localisation.html
-   Localisation assistance for jQuery v1.0.1.
+/* http://keith-wood.name/localisation.html
+   Localisation assistance for jQuery v1.0.2.
    Written by Keith Wood (kbwood@iprimus.com.au) June 2007. 
    Under the Creative Commons Licence http://creativecommons.org/licenses/by/3.0/
    Share or Remix it but please Attribute the author. */
@@ -11,16 +11,16 @@
    and loads them in order from least to most specific.
    For example, $.localise('jquery-calendar');
    with the browser set to 'en-US' would attempt to load
-   jquery-calendar-en.js and jquery-calendar-en-us.js.
+   jquery-calendar-en.js and jquery-calendar-en-US.js.
    Also accepts an array of package names to process.
    Optionally specify whether or not to include the base file,
    the desired language, and/or the timeout period, e.g.
    $.localise(['jquery-calendar', 'jquery-timeentry'], 
-      {loadBase: true; language: 'en-au', timeout: 300}); */
+      {loadBase: true; language: 'en-AU', timeout: 300}); */
 $.localise = function(pkg, settings) {
-	saveSettings = {async: $.ajaxSettings.async, timeout: $.ajaxSettings.timeout};
+	var saveSettings = {async: $.ajaxSettings.async, timeout: $.ajaxSettings.timeout};
 	$.ajaxSetup({async: false, timeout: (settings && settings.timeout ? settings.timeout : 500)});
-	localiseOne = function(pkg, lang) {
+	var localiseOne = function(pkg, lang) {
 		if (settings && settings.loadBase) {
 			$.getScript(pkg + '.js');
 		}
@@ -31,7 +31,7 @@ $.localise = function(pkg, settings) {
 			$.getScript(pkg + '-' + lang.substring(0, 5) + '.js');
 		}
 	};
-	lang = (settings && settings.language ? settings.language : $.defaultLanguage).toLowerCase();
+	var lang = normaliseLang(settings && settings.language ? settings.language : $.defaultLanguage);
 	if (isArray(pkg)) {
 		for (i = 0; i < pkg.length; i++) {
 			localiseOne(pkg[i], lang);
@@ -44,7 +44,17 @@ $.localise = function(pkg, settings) {
 };
 
 /* Retrieve the default language set for the browser. */
-$.defaultLanguage = (navigator.language ? navigator.language /* Mozilla */ : navigator.userLanguage /* IE */);
+$.defaultLanguage = normaliseLang(navigator.language ? navigator.language /* Mozilla */ :
+	navigator.userLanguage /* IE */);
+
+/* Ensure language code is in the format aa-AA. */
+function normaliseLang(lang) {
+	lang = lang.replace(/_/, '-').toLowerCase();
+	if (lang.length > 3) {
+		lang = lang.substring(0, 3) + lang.substring(3).toUpperCase();
+	}
+	return lang;
+}
 
 /* Determine whether an object is an array. */
 function isArray(a) {
